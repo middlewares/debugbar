1abcd9b673ad1d69af7af42ace68aa4b85c18c55
oscarotero <oom@oscarotero.com>
2018-01-24 21:21:16 +0100

    Code style fix

--- src/Debugbar.php
+++ src/Debugbar.php
@@ -5,10 +5,10 @@ namespace Middlewares;
 
 use DebugBar\DebugBar as Bar;
 use DebugBar\StandardDebugBar;
-use Psr\Http\Server\MiddlewareInterface;
-use Psr\Http\Server\RequestHandlerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 
 class Debugbar implements MiddlewareInterface
 {
